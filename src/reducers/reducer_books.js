a11983cc563a3f933a951e9caaa12e550aa8dfcd
Hadi Yousufi <reach.hadiy@gmail.com>
2018-03-22 01:02:50 -0500

    adds book-list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "harry Potter"},
+        {title: "Eloquent Ruby"},
+        {title: "The Dark tower"},
+        {title: "JS the Good parts"},
+    ]
+}
